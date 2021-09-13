a96230e36483b9c35ba8e1d0e73e020c6c41482b
benjaminpants <maltbyfamily@gmail.com>
2021-09-13 09:13:21 -0700

    Update Config.hx

--- source/Config.hx
+++ source/Config.hx
@@ -15,7 +15,6 @@ class Config
 
     public static var PixelStages:Array<String> = [ "senpai", "roses", "thorns" ];
 
-
     public static var DialogueCharacters:Map<String,DialogueCharacter> = [
         "bf-pixel" => new DialogueCharacter("weeb/bfPortrait",'Boyfriend portrait enter',false,"pixel_",1,true),
         "senpai" => new DialogueCharacter("weeb/senpaiPortrait",'Senpai Portrait Enter',true,"pixel_",1,true),
